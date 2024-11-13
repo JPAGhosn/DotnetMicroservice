f1e64c978a7614361cbc94f1df8619ff9f8583eb
Jean Paul Abi Ghosn <jabighosn@kurkle.com>
2024-11-13 08:07:50 +0200

    .

--- Backend/Solution Items/Docker/Production/profiles-service.Production.sh
+++ Backend/Solution Items/Docker/Production/profiles-service.Production.sh
@@ -3,8 +3,9 @@
 REGISTRY_URL="localhost:9870";
 
 docker build \
-  -t ${REGISTRY_URL}/kurkle/profilesservice \
+  -t ${REGISTRY_URL}/kurkle/profiles-service \
   -f ../../../Profiles/Dockerfile ../../.. \
   --build-arg BUILD_CONFIGURATION=Production;
   
-docker push ${REGISTRY_URL}/kurkle/profiles
+docker push ${REGISTRY_URL}/kurkle/profiles-service
+ 
