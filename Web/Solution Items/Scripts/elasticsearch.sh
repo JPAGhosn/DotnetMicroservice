f79741b9a77dfe5c8311938e9018a06e9be9ad24
Jean Paul Abi Ghosn <jabighosn@kurkle.com>
2024-12-03 07:40:20 +0200

    .

--- Web/Solution Items/Scripts/elasticsearch.sh
+++ Web/Solution Items/Scripts/elasticsearch.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-kubectl apply -f ../../K8S/ElasticSearch/service.yaml;
+kubectl apply -f ../K8S/ElasticSearch/elasticsearch.yaml;
+kubectl apply -f ../K8S/ElasticSearch/elasticsearch-nodeport.yaml;
+kubectl apply -f ../K8S/ElasticSearch/kibana.yaml;
+kubectl apply -f ../K8S/ElasticSearch/kibana-nodeport.yaml;
 
